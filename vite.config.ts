2877ef5f7bbfd10f2870f950cb2fae788a285a7c
itsyoboieltr <72046715+itsyoboieltr@users.noreply.github.com>
2022-05-14 17:56:39 +0200

    feat: prod release

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/aics2022/',
+  base: process.env.VITE_BASE ?? '/',
   plugins: [react()],
 });
